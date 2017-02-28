9b101c554b599b0aa69a6d2bc5001d386ddcd1b9
Grant Atkins <gatkins@cs.odu.edu>
2017-02-28 00:02:45 -0500

    Init ParseLinks Class

---
+++ CS411Crystal/ParseLinks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS411Crystal
+{
+    public class ParseLinks
+    {
+        public string url { get; set; }
+
+        ParseLinks(string url)
+        {
+            this.url = url;
+        }
+
+        void requestUrl(string url)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this.url);
+            request.AllowAutoRedirect = true;
+            request.UserAgent = "Mozila/5.0";
+            request.Timeout = 10000;    //10s
+
+            HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
+
+            if (isAlive((int)resp.StatusCode))
+            {
+                Console.WriteLine("AWESOME ITS ALIVE");
+            }else
+            {
+                Console.WriteLine("NOT ALIVE");
+            }
+            // Display the status.  
+            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+        }
+
+        // Helpers
+        private bool isAbsoluteUrl(string url)
+        {
+            Uri result;
+            return Uri.TryCreate(url, UriKind.Absolute, out result);
+        }
+
+        private bool isAlive(int code)
+        {
+            if(code == 200)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
