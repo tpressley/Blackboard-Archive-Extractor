315d86a0c4fa11515387bb0f3db1584d24b36b31
Grant Atkins <gatkins@cs.odu.edu>
2017-03-06 00:16:48 -0500

    Added test class to ParseLinks. Altered ParseLinks class

--- BlackboardArchiveExtractorTests/ParseLinksTests.cs
+++ BlackboardArchiveExtractorTests/ParseLinksTests.cs
@@ -11,17 +11,54 @@ namespace CS411Crystal.Tests
     [TestClass()]
     public class ParseLinksTests
     {
+
+        /// <summary>
+        /// Constructor Test
+        /// </summary>
         [TestMethod()]
         public void ParseLinksTest()
         {
-            Assert.Fail();
+            ParseLinks parser = new ParseLinks("http://www.cs.odu.edu/~gatkins");
+            Assert.AreEqual(parser.uri, "http://www.cs.odu.edu/~gatkins");
+            Assert.AreEqual(parser.finalUri, "");
         }
 
+        /// <summary>
+        /// Request Test
+        /// </summary>
         [TestMethod()]
         public void requestUrlTest()
         {
+            // init and check valid uri
             ParseLinks parser = new ParseLinks("http://www.cs.odu.edu/~gatkins");
-            Assert.Fail();
+            bool isAlive = parser.requestUrl();
+            Assert.AreEqual(isAlive,true);
+            // check redirect
+            parser = new ParseLinks("http://www.cs.odu.edu/~gatkins/cs532/redirect.php");
+            isAlive = parser.requestUrl();
+            Assert.AreEqual(isAlive, false);
+            // check infinite redirect error
+            parser = new ParseLinks("http://www.cs.odu.edu/~gatkins/cs532/redirect2.php");
+            isAlive = parser.requestUrl();
+            Assert.AreEqual(isAlive, false);
+        }
+
+        /// <summary>
+        /// Test isAbsoluteUri Function. Check relative path, absolute path, and no path reference
+        /// Only absolute path should return true.
+        /// </summary>
+        [TestMethod()]
+        public void AbsolutePathTest()
+        {
+            ParseLinks parser = new ParseLinks("http://www.cs.odu.edu/~gatkins");
+            bool relativePath = parser.isAbsoluteUri("./test.html");
+            Assert.AreEqual(relativePath,false);
+
+            bool absolutePath = parser.isAbsoluteUri("http://www.cs.odu.edu");
+            Assert.AreEqual(absolutePath, true);
+
+            bool errorTest = parser.isAbsoluteUri("");
+            Assert.AreEqual(errorTest, false);
         }
     }
 }
--- CS411Crystal/ParseLinks.cs
+++ CS411Crystal/ParseLinks.cs
@@ -9,39 +9,78 @@ namespace CS411Crystal
 {
     public class ParseLinks
     {
-        public string url { get; set; }
+        public string uri { get; set; }                 // initial URI passed to this class to start request
+        public string finalUri { get; private set; }    // final URI that the response returns
 
-        public ParseLinks(string url)
+        public ParseLinks(string uri)
         {
-            this.url = url;
+            this.uri = uri;
+            this.finalUri = "";
         }
 
-        public void requestUrl(string url)
+        /// <summary>
+        /// HTTP GET request function for URI. 
+        /// Returns true if resp code is 200 otherwise considered dead.
+        /// Some things it should be equipped to handle is Monitor Connectivity.
+        /// </summary>
+        /// <returns>Boolean of request if alive or not</returns>
+        public bool requestUrl()
         {
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this.url);
-            request.AllowAutoRedirect = true;
-            request.UserAgent = "Mozila/5.0";
-            request.Timeout = 10000;    //10s
-
-            HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
-
-            if (isAlive((int)resp.StatusCode))
+            try
             {
-                Console.WriteLine("AWESOME ITS ALIVE");
-            }else
-            {
-                Console.WriteLine("NOT ALIVE");
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this.uri);
+                request.AllowAutoRedirect = true;
+                request.UserAgent = "Mozila/5.0";
+                request.Timeout = 10000;    //10s
+
+                HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
+                finalUri = resp.ResponseUri.ToString();
+                Console.WriteLine((int)resp.StatusCode);
+
+                if (isAlive((int)resp.StatusCode))
+                {
+                    Console.WriteLine("ENDED WITH 200");
+                    return true;
+                }
+                return false;
             }
-            // Display the status.  
-            //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            catch (WebException e)
+            {
+                Console.WriteLine("ERR: " + e.ToString());
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {   
+
+                    var response = e.Response as HttpWebResponse;
+                    if (response != null)
+                    {
+                        var statusCode = (int)response.StatusCode;
+                        Console.WriteLine("HTTP Status Code: " + statusCode);
+                        return isAlive(statusCode);
+                    }
+                    else
+                    {
+                        return false;
+                        // no http status code available
+                    }
+                }
+                else
+                {
+                    return false;
+                    // no http status code available
+                }
+
+            }
         }
 
-        // Helpers
-        private bool isAbsoluteUrl(string url)
+        /// <summary>
+        /// Helper to check if Uri is an absolute path
+        /// </summary>
+        /// <param name="uri">URI to be checked if it is an absolute URI</param>
+        /// <returns>Boolean of whether it is a an absolute URI or not</returns>
+        public bool isAbsoluteUri(string uri)
         {
             Uri result;
-            return Uri.TryCreate(url, UriKind.Absolute, out result);
+            return Uri.TryCreate(uri, UriKind.Absolute, out result);
         }
 
         private bool isAlive(int code)
