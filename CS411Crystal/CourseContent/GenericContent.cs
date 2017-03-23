8020df3a1019697fb462df2d36eef2a1b826c133
tpressle <tpressle@cs.odu.edu>
2017-03-23 15:45:08 -0400

    Improved orgnization of files

---
+++ CS411Crystal/CourseContent/GenericContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS411Crystal
+{
+    class GenericContent : CourseContent
+    {
+        public string InnerText { get; set; }
+        private List<IBlackBoardResource> resources = new List<IBlackBoardResource>();
+
+        public List<IBlackBoardResource> Resources
+        {
+            get { return resources; }
+        }
+    }
+}
---
+++ CS411Crystal/Resources/ImageResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CS411Crystal
+{
+    class ImageResource : IBlackBoardResource
+    {
+        private byte[] pictureByteStream;
+        private Image picture;
+
+        public Image Picture
+        {
+            get { return picture; }
+            set { picture = value; }
+        }
+    }
+}
