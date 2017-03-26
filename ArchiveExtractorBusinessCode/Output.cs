7d45860132e2f75b88d42a5024628d8d2b28859a
Chris Soffos <csoff001@odu.edu>
2017-03-26 17:19:53 -0400

    I don't know what I'm doing

---
+++ ArchiveExtractorBusinessCode/Output.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace ArchiveExtractorBusinessCode
+{
+    public class Output
+    {
+        public string TargetDir { get; set; }
+        
+        public Output(string targetDir)
+        {
+            TargetDir = targetDir;
+        }
+
+        public bool CreateDir()
+        {
+            try
+            {
+                if (Directory.Exists(TargetDir))
+                {
+                    return false;
+                }
+                var dirInfo = Directory.CreateDirectory(TargetDir);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+            return true;
+        }
+        public bool CreateDir(string targetDir)
+        {
+            TargetDir = targetDir;
+            try
+            {
+                CreateDir();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+            return true;
+        }
+        public bool CreateIndex(System.Collections.Generic.List<Object> elements)
+        {
+            //Get dictionary since all elements may not be sorted
+            //Want to keep like files together
+            string tableContent = "";
+            
+            foreach(Object obj in elements)
+            {
+                tableContent += "<tr><td>" + obj.ToString() + "</td><td>" + obj.GetType() + "</td></tr>\n";
+            }
+
+            //Grab index template
+            string indexString = "";
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "CS411Crystal.ArchiveExtractorBusinessCode.StaticFiles.index.html";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                indexString = reader.ReadToEnd();
+            }
+
+            //Replace templating portions with variables retrieved
+            indexString = indexString.Replace("{INDEX_TITLE}", "BAE Index File");
+            indexString = indexString.Replace("{INDEX_TABLE_CONTENT}", tableContent);
+
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(TargetDir + "\\index.html");
+                file.WriteLine(indexString);
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
