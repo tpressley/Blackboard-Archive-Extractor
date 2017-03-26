398e2f3e06a6356c7b99d8352ea06523263d2820
Tristan Pressley <trspres@gmail.com>
2017-03-26 15:23:25 -0400

    Created CLI project

---
+++ ArchiveExtractorCLI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchiveExtractorBusinessCode;
+
+namespace ArchiveExtractorCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ArchiveLocation = "";
+            string ExtractDestination = "";
+
+            try
+            {
+                ArchiveLocation = args[0];
+                ExtractDestination = args[1];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine("Usage: ArchiveExtractor 'ArchiveToExtract' 'TargetLocation'");
+            }
+        }
+    }
+}
