334b3f6d303e435e36069f63e96ca9b70ee68570
Tristan Pressley <trspres@gmail.com>
2017-04-13 16:43:35 -0400

    Added missing files

---
+++ ArchiveExtractorBusinessCode/Resources/TextResource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ArchiveExtractorBusinessCode.Resources
+{
+    class TextResource : IBlackBoardResource
+    {
+        public string RefId { get; set; }
+        public string Text { get; set; }
+        public string PathToResourceFile { get; set; }
+
+        public TextResource(string text, string refId)
+        {
+            RefId = refId;
+            Text = text;
+        }
+
+        public TextResource(string PathToResourceFile)
+        {
+            string xml = File.ReadAllText(PathToResourceFile);
+            XElement xele = XElement.Parse(xml);
+            if (xele.Descendants("TEXT").Any())
+            {
+                List<XElement> texts = xele.Descendants("TEXT").ToList();
+                this.Text = texts[0].Value;
+            }
+            else
+            {
+                this.Text = "";
+            }
+            this.RefId = Path.GetFileNameWithoutExtension(PathToResourceFile);
+            this.PathToResourceFile = PathToResourceFile;
+        }
+    }
+}
