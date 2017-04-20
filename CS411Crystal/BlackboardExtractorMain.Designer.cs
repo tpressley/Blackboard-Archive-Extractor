8b63b0dc014c390eeacd2f252118fb6b9484efb2
Tristan Pressley <trspres@gmail.com>
2017-04-19 20:32:45 -0400

    Added GUI functions

--- CS411Crystal/BlackboardExtractorMain.Designer.cs
+++ CS411Crystal/BlackboardExtractorMain.Designer.cs
@@ -30,12 +30,57 @@ namespace CS411Crystal
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbxSourcePath = new System.Windows.Forms.TextBox();
+            this.tbxDestination = new System.Windows.Forms.TextBox();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxSourcePath
+            // 
+            this.tbxSourcePath.Location = new System.Drawing.Point(12, 12);
+            this.tbxSourcePath.Name = "tbxSourcePath";
+            this.tbxSourcePath.Size = new System.Drawing.Size(143, 20);
+            this.tbxSourcePath.TabIndex = 0;
+            this.tbxSourcePath.Text = "Source Path";
+            // 
+            // tbxDestination
+            // 
+            this.tbxDestination.Location = new System.Drawing.Point(161, 12);
+            this.tbxDestination.Name = "tbxDestination";
+            this.tbxDestination.Size = new System.Drawing.Size(143, 20);
+            this.tbxDestination.TabIndex = 1;
+            this.tbxDestination.Text = "Destination Path";
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(229, 38);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(75, 23);
+            this.btnExtract.TabIndex = 2;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // BlackboardExtractorMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 74);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.tbxDestination);
+            this.Controls.Add(this.tbxSourcePath);
+            this.Name = "BlackboardExtractorMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbxSourcePath;
+        private System.Windows.Forms.TextBox tbxDestination;
+        private System.Windows.Forms.Button btnExtract;
     }
 }
 
--- CS411Crystal/BlackboardExtractorMain.cs
+++ CS411Crystal/BlackboardExtractorMain.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using ArchiveExtractorBusinessCode;
 
 namespace CS411Crystal
 {
@@ -7,6 +12,41 @@ namespace CS411Crystal
         public BlackboardExtractorMain()
         {
             InitializeComponent();
+        }
+
+        private void btnExtract_Click(object sender, System.EventArgs e)
+        {
+            var archiveLocation = "";
+            var extractDestination = "";
+            var tempLocation = "";
+
+            archiveLocation = tbxSourcePath.Text;
+            extractDestination = tbxDestination.Text;
+            tempLocation = extractDestination + @"/Archive";
+
+            if (Directory.Exists(tempLocation))
+            {
+                Directory.Delete(tempLocation, true);
+            }
+
+            Console.WriteLine("Extracting Zip...");
+            Archive.ExtractArchive(archiveLocation, tempLocation);
+            Console.WriteLine("Done");
+            var xml = File.ReadAllText(tempLocation + "/imsmanifest.xml");
+            XElement manifest = XElement.Parse(xml);
+
+
+            List<XElement> xele = ManifestParser.GetOrganizationElements(manifest);
+            List<CourseContent> course = new List<CourseContent>();
+            foreach (XElement x in xele)
+            {
+                Console.WriteLine(x);
+                CourseContent cc = new CourseContent(x, tempLocation);
+                course.Add(cc);
+            }
+            Output.CreateRootIndex(course, extractDestination);
+            Directory.Delete(tempLocation, true);
+            System.Console.ReadKey();
         }
     }
 }
