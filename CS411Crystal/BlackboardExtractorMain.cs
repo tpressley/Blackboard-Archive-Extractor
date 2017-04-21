957d991ddb46ed24c7d1ab336bc2a15b2e65f5b4
Tristan Pressley <trspres@gmail.com>
2017-04-20 21:31:19 -0400

    Add missing changes

--- CS411Crystal/BlackboardExtractorMain.cs
+++ CS411Crystal/BlackboardExtractorMain.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using ArchiveExtractorBusinessCode;
 
 namespace CS411Crystal
 {
